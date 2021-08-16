f187add0dfa044918e44595c095ac522848213a8
Rafael BSIDES Pereira <rafaelbsides at gmail>
2021-08-16 01:34:14 -0300

    Conserta bug do login

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <div className="relative mx-auto h-12 w-auto -m-8">
+          <div className="relative mx-auto h-12 w-auto">
             <Image
               className=""
               src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
