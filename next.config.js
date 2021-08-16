fc8596ac73878928db0bdfa19fd464c2ef104938
Rafael BSIDES Pereira <rafaelbsides at gmail>
2021-08-16 03:15:27 -0300

    Adita pacote pra adicionar svg e pagina de produto

--- next.config.js
+++ next.config.js
@@ -5,4 +5,24 @@ module.exports = {
     domains: ['tailwindui.com', 'images.unsplash.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      // issuer section restricts svg as component only to
+      // svgs imported from js / ts files.
+      //
+      // This allows configuring other behavior for
+      // svgs imported from other file types (such as .css)
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+          },
+        },
+      ],
+    })
+    return config
+  },
 }
